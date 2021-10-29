23bda5c8a0db9bdbd27b5162780968ed83688b48
Raghu Ramesha <rags1357@gmail.com>
2021-10-29 12:53:08 -0700

    Update on-jupyter-server-start.sh

--- scripts/install-autoshutdown-server-extension/on-jupyter-server-start.sh
+++ scripts/install-autoshutdown-server-extension/on-jupyter-server-start.sh
@@ -43,7 +43,7 @@ chmod +x .auto-shutdown/set-time-interval.sh
 # "wget" is not part of the base Jupyter Server image, you need to install it first if needed to download the tarball
 sudo yum install -y wget
 # You can download the tarball from GitHub or alternatively, if you're using VPCOnly mode, you can host on S3
-wget -O .auto-shutdown/extension.tar.gz https://github.com/aws-samples/sagemaker-studio-auto-shutdown-extension/raw/main/sagemaker_studio_autoshutdown-0.1.6.tar.gz
+wget -O .auto-shutdown/extension.tar.gz https://github.com/aws-samples/sagemaker-studio-auto-shutdown-extension/raw/main/sagemaker_studio_autoshutdown-0.1.5.tar.gz
 
 # Or instead, could serve the tarball from an S3 bucket in which case "wget" would not be needed:
 # aws s3 --endpoint-url [S3 Interface Endpoint] cp s3://[tarball location] .
@@ -51,7 +51,7 @@ wget -O .auto-shutdown/extension.tar.gz https://github.com/aws-samples/sagemaker
 # Installs the extension
 cd .auto-shutdown
 tar xzf extension.tar.gz
-cd sagemaker_studio_autoshutdown-0.1.6
+cd sagemaker_studio_autoshutdown-0.1.5
 pip install --no-dependencies --no-build-isolation -e .
 jupyter serverextension enable --py sagemaker_studio_autoshutdown
 
