41df236fbb89b51655655b0d7065612ad8124c7c
Naresh Kumar Kolloju <kollojun@amazon.com>
2022-05-06 12:07:59 -0700

    Adds support for activating conda environment
    tested that this work in new enviroments

--- scripts/install-autoshutdown-server-extension/on-jupyter-server-start.sh
+++ scripts/install-autoshutdown-server-extension/on-jupyter-server-start.sh
@@ -52,8 +52,19 @@ wget -O .auto-shutdown/extension.tar.gz https://github.com/aws-samples/sagemaker
 cd .auto-shutdown
 tar xzf extension.tar.gz
 cd sagemaker_studio_autoshutdown-0.1.5
+
+# Activate studio environment just for installing extension
+export AWS_SAGEMAKER_JUPYTERSERVER_IMAGE="${AWS_SAGEMAKER_JUPYTERSERVER_IMAGE:-'jupyterserver'}"
+studio_conda_env_exists(){ conda env list | grep 'studio' >/dev/null 2>&1; }
+if [ "$AWS_SAGEMAKER_JUPYTERSERVER_IMAGE" = "jupyterserver-3" ] || studio_conda_env_exists ; then
+    eval "$(conda shell.bash hook)"
+    conda activate studio
+fi;
 pip install --no-dependencies --no-build-isolation -e .
 jupyter serverextension enable --py sagemaker_studio_autoshutdown
+if [ "$AWS_SAGEMAKER_JUPYTERSERVER_IMAGE" = "jupyterserver-3" ] || studio_conda_env_exists ; then
+    conda deactivate
+fi;
 
 # Restarts the jupyter server
 nohup supervisorctl -c /etc/supervisor/conf.d/supervisord.conf restart jupyterlabserver
