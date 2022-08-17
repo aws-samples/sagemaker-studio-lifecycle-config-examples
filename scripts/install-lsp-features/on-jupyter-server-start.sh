432a351690aa68126f965b40fdc2ceb18c2b7411
Sean Morgan <seanmrgn@amazon.com>
2022-08-16 17:32:54 -0700

    Add support for jump to definition source files in studio

--- scripts/install-lsp-features/on-jupyter-server-start.sh
+++ scripts/install-lsp-features/on-jupyter-server-start.sh
@@ -33,6 +33,16 @@ pip install jupyterlab-lsp \
 # https://jupyterlab-lsp.readthedocs.io/en/latest/Language%20Servers.html
 jlpm add --dev bash-language-server dockerfile-language-server-nodejs
 
+# Install sagemaker SDK  & scikit-learn onto jlab server for jump to definition support for those libraries
+pip install sagemaker scikit-learn
+# Create an lsp symlink directory so jupyterlab can open source-files not found in user home directory
+mkdir .lsp_symlink
+ln -s /opt .lsp_symlink/opt
+# Enable opening of symlink files found in the hidden director `.lsp_symlink/`
+echo yes | jupyter server --generate-config
+sed -i '1i c.ContentsManager.allow_hidden = True' .jupyter/jupyter_server_config.py
+
+
 # This configuration override is optional, to make LSP "extra-helpful" by default:
 CMP_CONFIG_DIR=.jupyter/lab/user-settings/@krassowski/jupyterlab-lsp/
 CMP_CONFIG_FILE=completion.jupyterlab-settings
